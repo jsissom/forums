fa9621245d27cfd2c7a2ed0d2fca027ed69ebf1d
Jay Sissom <jsissom@jsissom.com>
2013-10-08 21:23:45 -0400

    Initial checkin

---
+++ README.md
@@ -0,0 +1,11 @@
+== README
+
+This is a test application.  When finished it will:
+
+* Support user creation, management and login
+
+* Admin users
+
+* Have a message board that has subject areas and topics
+
+* Support markdown for message format
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Forums::Application.config.secret_key_base = '6a7441411d67801000589b4ae5b6dfe9c140360ff288fa5fab1df139a3f9ff893ee977e851774ea8f0a584246db4480dfa5811e1a4581e8e9bb30ce7a491db06'
